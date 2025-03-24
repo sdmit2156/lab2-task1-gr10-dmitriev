c261bd2519c8c3d384e1a857f6a0ea45585839b2
sdmit2156 <sdmitriev01052006@gmail.com>
2025-03-24 17:04:27 +0000

    added function

--- src/main.c
+++ src/main.c
@@ -5,3 +5,26 @@ typedef struct {
 		char brand[50];
 		int price;
 } Car;
+
+Car cars[N] = {
+		{"Toyota", 1500000},
+		{"BMW", 3500000},
+		{"Lada", 800000},
+		{"Kia", 1200000},
+		{"Mercedes", 4000000},
+		{"Honda", 1300000},
+		{"Audi", 3200000}
+};
+
+void sortCarsByPrice(Car cars[], int n) {
+		for (int i = 0; i < n - 1; i++) {
+				for (int j = 0; j < n - i - 1; j++) {
+						if (cars[j].price > cars[j + 1].price) {
+								// Обмен местами
+								Car temp = cars[j];
+								cars[j] = cars[j + 1];
+								cars[j + 1] = temp;
+						}
+				}
+		}
+}
